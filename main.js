c6e29072e5e8a8fa6c90fc5134c266ce3dce589c
Alex <89929071+AlexBreskolenko@users.noreply.github.com>
2024-07-04 21:17:34 +0300

    Task 2 completed

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
-//**************Task 1 */
-const numberAnswers = Number(
-  prompt("Введите число которое хотите возвести в куб:")
-);
+//**************Task 1
+// const numberAnswers = Number(
+//   prompt("Введите число которое хотите возвести в куб:")
+// );
+// function numberCubing(numberAnswers) {
+//   return numberAnswers * numberAnswers * numberAnswers;
+// }
+// console.log(
+//   `${numberAnswers} возведённое в куб = ${numberCubing(numberAnswers)}`
+// );
+//************Task 2
+const amountSalary = Number(prompt("Введите размер заработной платы:"));
 
-function numberCubing(numberAnswers) {
-  return numberAnswers * numberAnswers * numberAnswers;
+const taxDeduction = (amount) => amountSalary - (amount / 100) * 13;
+
+if (isNaN(amountSalary)) {
+  alert("Значение задано неверно.");
+} else {
+  alert(
+    `Размер заработной платы за вычетом налогов равен ${taxDeduction(
+      amountSalary
+    )}`
+  );
 }
-
-console.log(
-  `${numberAnswers} возведённое в куб = ${numberCubing(numberAnswers)}`
-);
