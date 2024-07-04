f5acdf5d3565a7391ef3e444420b5a2143ad3de4
Alex <89929071+AlexBreskolenko@users.noreply.github.com>
2024-07-04 20:41:23 +0300

    Task 1 completed

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
+//**************Task 1 */
+const numberAnswers = Number(
+  prompt("Введите число которое хотите возвести в куб:")
+);
 
+function numberCubing(numberAnswers) {
+  return numberAnswers * numberAnswers * numberAnswers;
+}
+
+console.log(
+  `${numberAnswers} возведённое в куб = ${numberCubing(numberAnswers)}`
+);
