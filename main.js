51ed5d8c5da21c4c36939c7b0401c3af7f34370a
Alex <89929071+AlexBreskolenko@users.noreply.github.com>
2024-07-04 21:56:51 +0300

    Task 4 completed

--- main.js
+++ main.js
@@ -23,14 +23,36 @@
 //   );
 // }
 //****************Task 3
+// const numberOne = Number(prompt("Введите первое число:"));
+// const numberTwo = Number(prompt("Введите второе число:"));
+// const numberThree = Number(prompt("Введите третье число:"));
+
+// const maxNumber = (numOne, numTwo, numThree) => {
+//   let max;
+//   numOne > numTwo ? (max = numOne) : (max = numTwo);
+//   return numThree > max ? (max = numThree) : (max = max);
+// };
+
+// if (isNaN(numberOne) || isNaN(numberTwo) || isNaN(numberThree)) {
+//   alert("Значение задано неверно.");
+// } else {
+//   alert(`Максимальное число = ${maxNumber(numberOne, numberTwo, numberThree)}`);
+// }
+//***************Task 4
 const numberOne = Number(prompt("Введите первое число:"));
 const numberTwo = Number(prompt("Введите второе число:"));
-const numberThree = Number(prompt("Введите третье число:"));
 
-const maxNumber = (numOne, numTwo, numThree) => {
-  let max;
-  numOne > numTwo ? (max = numOne) : (max = numTwo);
-  return numThree > max ? (max = numThree) : (max = max);
+const adding = (numOne, numTwo) => numOne + numTwo;
+const multiplicaton = (numOne, numTwo) => numOne * numTwo;
+const division = (numOne, numTwo) => numOne / numTwo;
+const difference = (numOne, numTwo) => {
+  if (numOne === numTwo) {
+    return 0;
+  } else if (numOne < numTwo) {
+    return numTwo - numOne;
+  } else {
+    return numOne - numTwo;
+  }
 };
 
-alert(`Максимальное число = ${maxNumber(numberOne, numberTwo, numberThree)}`);
+console.log(difference(numberOne, numberTwo));
