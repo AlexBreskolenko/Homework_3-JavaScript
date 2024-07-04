4f80a7fce8851b37bfe8fde606846ecc2ba751ae
Alex <89929071+AlexBreskolenko@users.noreply.github.com>
2024-07-04 21:34:56 +0300

    task 3 completed

--- main.js
+++ main.js
@@ -9,16 +9,28 @@
 //   `${numberAnswers} возведённое в куб = ${numberCubing(numberAnswers)}`
 // );
 //************Task 2
-const amountSalary = Number(prompt("Введите размер заработной платы:"));
+// const amountSalary = Number(prompt("Введите размер заработной платы:"));
 
-const taxDeduction = (amount) => amountSalary - (amount / 100) * 13;
+// const taxDeduction = (amount) => amountSalary - (amount / 100) * 13;
 
-if (isNaN(amountSalary)) {
-  alert("Значение задано неверно.");
-} else {
-  alert(
-    `Размер заработной платы за вычетом налогов равен ${taxDeduction(
-      amountSalary
-    )}`
-  );
-}
+// if (isNaN(amountSalary)) {
+//   alert("Значение задано неверно.");
+// } else {
+//   alert(
+//     `Размер заработной платы за вычетом налогов равен ${taxDeduction(
+//       amountSalary
+//     )}`
+//   );
+// }
+//****************Task 3
+const numberOne = Number(prompt("Введите первое число:"));
+const numberTwo = Number(prompt("Введите второе число:"));
+const numberThree = Number(prompt("Введите третье число:"));
+
+const maxNumber = (numOne, numTwo, numThree) => {
+  let max;
+  numOne > numTwo ? (max = numOne) : (max = numTwo);
+  return numThree > max ? (max = numThree) : (max = max);
+};
+
+alert(`Максимальное число = ${maxNumber(numberOne, numberTwo, numberThree)}`);
